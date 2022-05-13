bbeb26d6e6781542dbf2504e798f021eb54c3b09
bakcoder <gudwls45@gmail.com>
2022-05-14 03:28:53 +0900

    ADD: programmers 나머지가 1이 되는 수 찾기

---
+++ src/bakcoder/java/algorithm/programmers/Lessons87389.java
@@ -0,0 +1,13 @@
+package bakcoder.java.algorithm.programmers;
+
+public class Lessons87389 {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=2 ; i<n ; i++) {
+            if(n % i == 1) return i;
+        }
+        return answer;
+    }
+}
+
+//https://programmers.co.kr/learn/courses/30/lessons/87389
