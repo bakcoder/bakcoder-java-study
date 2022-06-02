a87460efd22ea1c4b500840e56c3de244a5b06b4
bakcoder <gudwls45@gmail.com>
2022-06-02 22:37:14 +0900

    ADD: programmers 두 정수 사이의 합

---
+++ src/bakcoder/java/algorithm/programmers/Lessons12912.java
@@ -0,0 +1,21 @@
+package bakcoder.java.algorithm.programmers;
+
+public class Lessons12912 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 3;
+        System.out.println(new Lessons12912().otherSolution(a,b));
+    }
+    public long solution(int a, int b) {
+        long answer = 0;
+        for(int i=Math.min(a, b) ; i<=Math.max(a, b) ; i++)
+            answer += i;
+        return answer;
+    }
+
+    public long otherSolution(int a, int b) {
+        int min = Math.min(a, b);
+        int max = Math.max(a, b);
+        return max*(max+1)/2 - min*(min-1)/2;
+    }
+}
