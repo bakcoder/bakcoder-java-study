bdd63f8276ac3a4e0de8086c6d1cc318ac33a45a
bakcoder <gudwls45@gmail.com>
2022-06-03 23:00:01 +0900

    ADD: programmers 문자열을 정수로 바꾸기

---
+++ src/bakcoder/java/algorithm/programmers/Lessons12925.java
@@ -0,0 +1,23 @@
+package bakcoder.java.algorithm.programmers;
+
+public class Lessons12925 {
+    public static void main(String[] args) {
+        String s = "100";
+        System.out.println(new Lessons12925().solution2(s));
+    }
+    public int solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+
+    public int solution2(String s) {
+        int sign = s.contains("-") ? -1 : 1;
+        int startIdx = sign < 0 ? 1 : s.contains("+") ? 1 : 0;
+        int answer = 0;
+        for(int i= startIdx ; i<s.length() ; i++) {
+            answer *= 10;
+            answer += s.charAt(i) - '0';
+        }
+        return answer * sign;
+    }
+}
